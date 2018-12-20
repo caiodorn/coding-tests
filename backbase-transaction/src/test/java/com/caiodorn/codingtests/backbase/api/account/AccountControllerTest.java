f5f4e096f5edf1b9f32fecfc954f60e2f568c529
Caio Antunes <caiodorn@gmail.com>
2018-12-20 00:08:20 -0200

    + Unit tests.

--- backbase-transaction/src/test/java/com/caiodorn/codingtests/backbase/api/account/AccountControllerTest.java
+++ backbase-transaction/src/test/java/com/caiodorn/codingtests/backbase/api/account/AccountControllerTest.java
@@ -1,4 +1,67 @@
 package com.caiodorn.codingtests.backbase.api.account;
 
+import com.caiodorn.codingtests.backbase.api.account.transaction.Transaction;
+import com.caiodorn.codingtests.backbase.api.account.transaction.TransactionService;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.math.BigDecimal;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.Mockito.*;
+
 public class AccountControllerTest {
+
+    TransactionService transactionServiceMock;
+    AccountController accountController;
+    List<Transaction> expectedTransactions;
+
+    @BeforeEach
+    public void setup() {
+        transactionServiceMock = mock(TransactionService.class);
+        accountController = new AccountController(transactionServiceMock);
+        expectedTransactions = new ArrayList<>();
+    }
+
+    @Test
+    public void givenTransactionTypeNotProvided_whenGetTransactions_thenShouldDelegateToGetTransactions() {
+        when(transactionServiceMock.getTransactions("id")).thenReturn(expectedTransactions);
+
+        List<Transaction> returnedTransactions = accountController.getTransactions("token", "id", null);
+
+        verify(transactionServiceMock, never()).getTransactionsByType(anyString(), anyString());
+        verify(transactionServiceMock, never()).getTotalAmountByTransactionType(anyString(), anyString());
+        verify(transactionServiceMock, times(1)).getTransactions("id");
+        assertEquals(expectedTransactions, returnedTransactions);
+    }
+
+    @Test
+    public void givenTransactionTypeProvided_whenGetTransactions_thenShouldDelegateToGetTransactionsByType() {
+        when(transactionServiceMock.getTransactionsByType("id", "type")).thenReturn(expectedTransactions);
+
+        List<Transaction> returnedTransactions = accountController.getTransactions("token", "id", "type");
+
+        verify(transactionServiceMock, times(1)).getTransactionsByType(anyString(), anyString());
+        verify(transactionServiceMock, never()).getTotalAmountByTransactionType(anyString(), anyString());
+        verify(transactionServiceMock, never()).getTransactions("id");
+        assertEquals(expectedTransactions, returnedTransactions);
+    }
+
+    @Test
+    public void shouldReturnTotalAmount_whenGetTotalAmountByTransactionType() {
+        when(transactionServiceMock.getTotalAmountByTransactionType("id", "type")).thenReturn(BigDecimal.TEN);
+
+        Map<String, BigDecimal> expectedResponse = new HashMap<>();
+        expectedResponse.put("total", BigDecimal.TEN);
+
+        Map<String, BigDecimal> response = accountController.getTotalAmountByType("token", "id", "type");
+
+        verify(transactionServiceMock, times(1)).getTotalAmountByTransactionType("id", "type");
+        assertEquals(expectedResponse.toString(), response.toString());
+    }
+
 }
